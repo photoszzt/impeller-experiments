110335315013547ed85837e58134371d36a5b9dd
Zhiting Zhu <zzt0215@gmail.com>
2025-02-03 03:05:34 -0600

    script to measure recovery
    Signed-off-by: Zhiting Zhu <zzt0215@gmail.com>

---
+++ nexmark_impeller/run_q8_failure_snapshot.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -x
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+WORKSPACE_DIR=$(realpath $SCRIPT_DIR/../../)
+DIR=q8_boki/mem
+
+cd $DIR
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper start-machines --use-spot-instances
+./update_docker.sh
+cd ../..
+
+TPS_PER_WORKER=(20000 24000 28000)
+NUM_WORKER=4
+DURATION=330
+WARM_DURATION=0
+APP=q8
+FLUSH_MS=100
+SRC_FLUSH_MS=100
+SNAPSHOT_S=10
+COMM_EVERY_MS=100
+
+cd ${DIR}
+for ((i = 0; i < 1; ++i)); do
+	for ((idx = 0; idx < ${#TPS_PER_WORKER[@]}; ++idx)); do
+		TPS=$(expr ${TPS_PER_WORKER[idx]} \* ${NUM_WORKER})
+		EVENTS=$(expr $TPS \* $DURATION)
+		echo ${APP}, ${DIR[k]}, ${EVENTS} events, ${TPS} tps
+		subdir=${DURATION}s_${WARM_DURATION}swarm_${FLUSH_MS}ms_src${SRC_FLUSH_MS}ms
+		./run_once.sh --app ${APP} \
+			--exp_dir ./${NUM_WORKER}src_snapshot_300/$subdir/$i/${TPS_PER_WORKER[idx]}tps_epoch/ \
+			--gua epoch --duration $DURATION --events_num ${EVENTS} --nworker ${NUM_WORKER} \
+			--tps ${TPS} --warm_duration ${WARM_DURATION} --flushms $FLUSH_MS \
+			--src_flushms $SRC_FLUSH_MS --fail true --snapshot_s ${SNAPSHOT_S} --comm_everyMs ${COMM_EVERY_MS}
+	done
+done
+cd -
+
+cd $DIR
+$WORKSPACE_DIR/research-helper-scripts/microservice_helper stop-machines
+cd ../..
