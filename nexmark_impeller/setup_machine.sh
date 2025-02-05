c7c1f337901b53af171b63dafbf308f5990bef52
Zhiting Zhu <zzt0215@gmail.com>
2025-02-05 04:33:29 -0600

    download machine
    Signed-off-by: Zhiting Zhu <zzt0215@gmail.com>

--- nexmark_impeller/setup_machine.sh
+++ nexmark_impeller/setup_machine.sh
@@ -15,14 +15,14 @@ ALL_HOSTS=$($HELPER_SCRIPT get-all-server-hosts --base-dir=$BASE_DIR)
 MANAGER_HOST=$($HELPER_SCRIPT get-docker-manager-host --base-dir=$BASE_DIR)
 DOCKER_VER=$(ssh -q $MANAGER_HOST -oStrictHostKeyChecking=no -- 'docker version -f "{{.Server.Version}}"')
 DOCKER_VER_MAJOR=$(echo "$DOCKER_VER" | cut -d'.' -f 1)
-if [ "${DOCKER_VER_MAJOR}" -ge 24 ]; then
-	exit 0
-fi
 
 i=0
 for HOST in $ALL_HOSTS; do
 	SSH_CMD="ssh -q $HOST -oStrictHostKeyChecking=no"
 	$SSH_CMD -- "sudo sysctl vm.overcommit_memory=1"
+	$SSH_CMD -- "[ -d /home/ubuntu/impeller-artifact ] || git clone --recurse-submodules -j8 https://github.com/ut-osa/impeller-artifact.git /home/ubuntu/impeller-artifact" &
+	pids[$i]=$!
+	i=$((i + 1))
 	$SSH_CMD -- "sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin jq" &
 	pids[$i]=$!
 	i=$((i + 1))
